195e7f3d335f9a746a6f097c97d367426120f24b
zahmo <zuser@nowhere.nw>
2025-02-27 12:33:53 +0200

    feat:added ElementAttributeDirective that allows native attribubtes to be added to an element

--- projects/ng-formworks-core/src/lib/widget-library/widget-library.module.ts
+++ projects/ng-formworks-core/src/lib/widget-library/widget-library.module.ts
@@ -1,13 +1,14 @@
-import { BASIC_WIDGETS } from './index';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ElementAttributeDirective } from './element-attribute.directive';
+import { BASIC_WIDGETS } from './index';
 import { OrderableDirective } from './orderable.directive';
 
 @NgModule({
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    declarations: [...BASIC_WIDGETS, OrderableDirective],
-    exports: [...BASIC_WIDGETS, OrderableDirective]
+    declarations: [...BASIC_WIDGETS, OrderableDirective,ElementAttributeDirective],
+    exports: [...BASIC_WIDGETS, OrderableDirective,ElementAttributeDirective]
 })
 export class WidgetLibraryModule {
 }
