094c8cffd5c2b428556b0ddca9120a8e9d527e1e
zahmo <zuser@nowhere.nw>
2025-04-14 20:13:33 +0200

    refactor:switched orderable directive for nxt-sortablejs
    # Conflicts:
    #	projects/ng-formworks-core/src/lib/widget-library/input.component.ts
    #	projects/ng-formworks-core/src/lib/widget-library/number.component.ts
    #	projects/ng-formworks-core/src/lib/widget-library/root.component.ts

--- projects/ng-formworks-core/src/lib/widget-library/root.component.ts
+++ projects/ng-formworks-core/src/lib/widget-library/root.component.ts
@@ -1,29 +1,40 @@
-import { Component, input, inject } from '@angular/core';
+import { Component, inject, input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { JsonSchemaFormService } from '../json-schema-form.service';
 
 
 @Component({
-    // tslint:disable-next-line:component-selector
-    selector: 'root-widget',
-    template: `
-    <div *ngFor="let layoutItem of layout(); let i = index"
-      [class.form-flex-item]="isFlexItem()"
-      [style.align-self]="(layoutItem.options || {})['align-self']"
-      [style.flex-basis]="getFlexAttribute(layoutItem, 'flex-basis')"
-      [style.flex-grow]="getFlexAttribute(layoutItem, 'flex-grow')"
-      [style.flex-shrink]="getFlexAttribute(layoutItem, 'flex-shrink')"
-      [style.order]="(layoutItem.options || {}).order">
-      <div
-        [dataIndex]="layoutItem?.arrayItem ? (dataIndex() || []).concat(i) : (dataIndex() || [])"
-        [layoutIndex]="(layoutIndex() || []).concat(i)"
-        [layoutNode]="layoutItem"
-        [orderable]="isDraggable(layoutItem)">
-        <select-framework-widget *ngIf="showWidget(layoutItem)"
+  // tslint:disable-next-line:component-selector
+  selector: 'root-widget',
+  template: `
+    <div #sortableContainter [nxtSortablejs]="layout()" [config]="sortableConfig" (init)="sortableInit($event)">
+      <div *ngFor="let layoutItem of layout(); let i = index"
+        [class.form-flex-item]="isFlexItem()"
+        [style.align-self]="(layoutItem.options || {})['align-self']"
+        [style.flex-basis]="getFlexAttribute(layoutItem, 'flex-basis')"
+        [style.flex-grow]="getFlexAttribute(layoutItem, 'flex-grow')"
+        [style.flex-shrink]="getFlexAttribute(layoutItem, 'flex-shrink')"
+        [style.order]="(layoutItem.options || {}).order"
+        [class.sortable-filter]="!isDraggable(layoutItem)"
+        >
+        <!--NB orderable directive is not used but has been left in for now and set to false
+          otherwise the compiler won't recognize dataIndex and other dependent attributes
+        -->
+        <div 
           [dataIndex]="layoutItem?.arrayItem ? (dataIndex() || []).concat(i) : (dataIndex() || [])"
           [layoutIndex]="(layoutIndex() || []).concat(i)"
-          [layoutNode]="layoutItem"></select-framework-widget>
+          [layoutNode]="layoutItem"
+          [orderable]="false"
+          [class.sortable-filter]="!isDraggable(layoutItem)"
+          >
+          <select-framework-widget *ngIf="showWidget(layoutItem)"
+            [dataIndex]="layoutItem?.arrayItem ? (dataIndex() || []).concat(i) : (dataIndex() || [])"
+            [layoutIndex]="(layoutIndex() || []).concat(i)"
+            [layoutNode]="layoutItem"></select-framework-widget>
+        </div>
       </div>
-    </div>`,
+    </div>
+    `,
   styles: [`
     [draggable=true] {
       transition: all 150ms cubic-bezier(.4, 0, .2, 1);
@@ -46,8 +57,11 @@ import { JsonSchemaFormService } from '../json-schema-form.service';
       position: relative; z-index: 20;
     }
   `],
+  standalone: false
 })
-export class RootComponent {
+export class RootComponent implements OnInit, OnDestroy {
+
+
   private jsf = inject(JsonSchemaFormService);
 
   options: any;
@@ -57,9 +71,47 @@ export class RootComponent {
   readonly isOrderable = input<boolean>(undefined);
   readonly isFlexItem = input(false);
 
+  // @ViewChild('sortableContainter', {})
+  // sortableContainterElt: ElementRef;
+
+  sortableObj: any;
+  sortableConfig:any={
+    filter:".sortable-filter",//needed to disable dragging on input range elements, class needs to be added to the element or its parent
+    preventOnFilter: false,//needed for input range elements slider do still work
+    onEnd: (/**Event*/evt)=> {
+      evt.newIndex // most likely why this event is used is to get the dragging element's current index
+      // same properties as onEnd
+      //console.log(`sortablejs event:${evt}`);
+      let srcInd=evt.oldIndex;
+      let trgInd=evt.newIndex;
+      let layoutItem=this.layout()[trgInd];
+      let dataInd=layoutItem?.arrayItem ? (this.dataIndex() || []).concat(trgInd) : (this.dataIndex() || []);
+      let layoutInd=(this.layoutIndex() || []).concat(trgInd)
+      let itemCtx:any={
+        dataIndex:()=>{return dataInd},
+        layoutIndex:()=>{return layoutInd},
+        layoutNode:()=>{return layoutItem},
+      }
+      //must set moveLayout to false as nxtSortable already moves it
+      this.jsf.moveArrayItem(itemCtx, evt.oldIndex, evt.newIndex,false);
+      
+    }
+  }
+  private sortableOptionsSubscription: Subscription;
+  sortableInit(sortable) {
+    this.sortableObj = sortable;
+  }
+
   isDraggable(node: any): boolean {
-    return node.arrayItem && node.type !== '$ref' &&
-      node.arrayItemType === 'list' && this.isOrderable() !== false;
+    let result=node.arrayItem && node.type !== '$ref' &&
+    node.arrayItemType === 'list' && this.isOrderable() !== false;
+    if (this.sortableObj) {
+      //this.sortableObj.option("disabled",true);
+      //this.sortableObj.option("sort",false);
+      //this.sortableObj.option("disabled",!result);
+    }
+
+    return result;
   }
 
   // Set attributes for flexbox child
@@ -70,7 +122,28 @@ export class RootComponent {
       (node.options || {})[attribute] || ['1', '1', 'auto'][index];
   }
 
+
   showWidget(layoutNode: any): boolean {
     return this.jsf.evaluateCondition(layoutNode, this.dataIndex());
+  }
+  ngOnInit(): void {
+    // Subscribe to the draggable state
+    this.sortableOptionsSubscription = this.jsf.sortableOptions$.subscribe(
+      (optsValue) => {
+        if (this.sortableObj) {
+          Object.keys(optsValue).forEach(opt=>{
+            let optVal=optsValue[opt];
+            this.sortableObj.option(opt,optVal);
+          })
+          //this.sortableObj.option("disabled",true);
+          //this.sortableObj.option("sort",false);
+        }
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    if (this.sortableOptionsSubscription) {
+      this.sortableOptionsSubscription.unsubscribe();
+    }
   }
 }
