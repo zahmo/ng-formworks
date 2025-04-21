11ec5b36b20dd89f5db411547b2b3b6b7ecf08a2
zahmo <zuser@nowhere.nw>
2025-04-21 22:56:23 +0200

    fix:use e.stopPropagation() in mousedown and touchstart as a workaround to prevent dragging on these inputs
    # Conflicts:
    #	projects/ng-formworks-material/src/lib/widgets/material-slider.component.ts

--- projects/ng-formworks-material/src/lib/widgets/material-number.component.ts
+++ projects/ng-formworks-material/src/lib/widgets/material-number.component.ts
@@ -30,6 +30,8 @@ import { JsonSchemaFormService } from '@ng-formworks/core';
         [type]="'number'"
         (blur)="options.showErrors = true"
         [attributes]="inputAttributes"
+        (mousedown)="onMouseDown($event)"
+        (touchstart)="onTouchStart($event)"  
         >
       <input matInput *ngIf="!boundControl"
         [attr.aria-describedby]="'control' + layoutNode()?._id + 'Status'"
@@ -48,6 +50,8 @@ import { JsonSchemaFormService } from '@ng-formworks/core';
         (input)="updateValue($event)"
         (blur)="options.showErrors = true"
         [attributes]="inputAttributes"
+        (mousedown)="onMouseDown($event)"
+        (touchstart)="onTouchStart($event)"  
         >
       <span matSuffix *ngIf="options?.suffix || options?.fieldAddonRight"
         [innerHTML]="options?.suffix || options?.fieldAddonRight"></span>
@@ -86,7 +90,17 @@ export class MaterialNumberComponent implements OnInit {
   get inputAttributes() {
      return this.options?.['x-inputAttributes'];
   }
-    
+ 
+    //TODO review:stopPropagation used as a workaround 
+    //to prevent dragging onMouseDown and onTouchStart events
+    onMouseDown(e){
+      e.stopPropagation();
+    }
+
+    onTouchStart(e){
+      e.stopPropagation();
+    }  
+  
   ngOnInit() {
     this.options = this.layoutNode().options || {};
     this.jsf.initializeControl(this);
