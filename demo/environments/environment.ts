2cb23cd82b7f4304040a8a206d55ed82e17ec6d1
zahmo <zuser@nowhere.nw>
2025-04-23 10:24:02 +0200

    build:auto updated environment info

--- demo/environments/environment.ts
+++ demo/environments/environment.ts
@@ -1,3 +1,3 @@
 
-    export const environment ={"production":false,"version":"18.5.6","angularVersion":"18.2.13","materialVersion":"18.2.14","cssClassPrefix":true}
+    export const environment ={"production":false,"version":"18.5.7","angularVersion":"18.2.13","materialVersion":"18.2.14","cssClassPrefix":true}
     
