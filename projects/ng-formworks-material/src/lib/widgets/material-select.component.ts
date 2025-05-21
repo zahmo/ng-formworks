6a2773bcc02d60fe570ddc06cf4eb832d27f8256
zahmo <zuser@nowhere.nw>
2025-05-21 21:35:56 +0200

    fix:prevent sorting and dragging of $ref component,usually an add button

--- projects/ng-formworks-material/src/lib/widgets/material-select.component.ts
+++ projects/ng-formworks-material/src/lib/widgets/material-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, input } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject, input } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { JsonSchemaFormService, buildTitleMap, isArray } from '@ng-formworks/core';
@@ -82,7 +82,7 @@ import { JsonSchemaFormService, buildTitleMap, isArray } from '@ng-formworks/cor
   `],
     standalone: false
 })
-export class MaterialSelectComponent implements OnInit {
+export class MaterialSelectComponent implements OnInit, OnDestroy {
   matFormFieldDefaultOptions = inject(MAT_FORM_FIELD_DEFAULT_OPTIONS, { optional: true });
   private jsf = inject(JsonSchemaFormService);
 
@@ -113,5 +113,9 @@ export class MaterialSelectComponent implements OnInit {
   updateValue(event) {
     this.options.showErrors = true;
     this.jsf.updateValue(this, event.value);
+  }
+
+  ngOnDestroy () {
+    this.jsf.updateValue(this, null);
   }
 }
