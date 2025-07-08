d3f2d9cafc188f484fcbf44e42c2fdf0d11ebc91
zahmo <zuser@nowhere.nw>
2025-07-08 16:33:47 +0200

    fix:updated material button widget ngOnDestroy() to clear value

--- projects/ng-formworks-material/src/lib/widgets/material-button.component.ts
+++ projects/ng-formworks-material/src/lib/widgets/material-button.component.ts
@@ -43,7 +43,7 @@ export class MaterialButtonComponent implements OnInit,OnDestroy {
   ngOnDestroy(): void {
     this.isValidChangesSubs?.unsubscribe();
     this.isValidChangesSubs=null;
-    this.updateValue({target:null});
+    this.updateValue({target:{value:null}});
   }
 
   ngOnInit() {
