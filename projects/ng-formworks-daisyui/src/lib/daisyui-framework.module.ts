2dd873f4563a2130ebc3f3e92f142dfa8fba61db
zahmo <zuser@nowhere.nw>
2025-07-08 16:43:55 +0200

    feat:added support for daisyui one-of widget

--- projects/ng-formworks-daisyui/src/lib/daisyui-framework.module.ts
+++ projects/ng-formworks-daisyui/src/lib/daisyui-framework.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Framework, FrameworkLibraryService, JsonSchemaFormModule, JsonSchemaFormService, WidgetLibraryModule, WidgetLibraryService } from '@ng-formworks/core';
 
+import { FormsModule } from '@angular/forms';
 import { CssFrameworkModule } from '@ng-formworks/cssframework';
 import { DaisyUIFrameworkComponent } from './daisyui-framework.component';
 import { DaisyUIFrameworkComponentPrefixed } from './daisyui-framework.prefixed.component';
 import { DaisyUIFramework } from './daisyui.framework';
+import { DaisyUIOneOfComponent } from './widgets/daisyui-one-of.component';
 import { DaisyUITabsComponent } from './widgets/daisyui-tabs.component';
 
 
@@ -14,11 +16,13 @@ import { DaisyUITabsComponent } from './widgets/daisyui-tabs.component';
   declarations: [
     DaisyUIFrameworkComponent,
     DaisyUIFrameworkComponentPrefixed,
-    DaisyUITabsComponent
+    DaisyUITabsComponent,
+    DaisyUIOneOfComponent
   ],
   imports: [
     JsonSchemaFormModule,
     CommonModule,
+    FormsModule,
     WidgetLibraryModule,
     CssFrameworkModule
   ],
