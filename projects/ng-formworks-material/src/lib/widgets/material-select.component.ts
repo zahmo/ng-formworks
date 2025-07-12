78a76cc21959c722e0b4354637cb272a2384ec77
zahmo <zuser@nowhere.nw>
2025-07-12 17:13:51 +0200

    feat:added support for multiselect
    # Conflicts:
    #	projects/ng-formworks-core/src/lib/widget-library/select.component.ts
    #	projects/ng-formworks-material/src/lib/widgets/material-select.component.ts

--- projects/ng-formworks-material/src/lib/widgets/material-select.component.ts
+++ projects/ng-formworks-material/src/lib/widgets/material-select.component.ts
@@ -16,7 +16,10 @@ import { JsonSchemaFormService, buildTitleMap, isArray } from '@ng-formworks/cor
       <mat-label *ngIf="!options?.notitle">{{options?.title}}</mat-label>
       <span matPrefix *ngIf="options?.prefix || options?.fieldAddonLeft"
         [innerHTML]="options?.prefix || options?.fieldAddonLeft"></span>
-      <mat-select *ngIf="boundControl"
+      <ng-container *ngIf="boundControl">
+      
+      </ng-container>  
+      <mat-select *ngIf="boundControl && !options?.multiple"
         [formControl]="formControl"
         [attr.aria-describedby]="'control' + layoutNode()?._id + 'Status'"
         [attr.name]="controlName"
@@ -25,7 +28,8 @@ import { JsonSchemaFormService, buildTitleMap, isArray } from '@ng-formworks/cor
         [placeholder]="options?.notitle ? options?.placeholder : options?.title"
         [required]="options?.required"
         [style.width]="'100%'"
-        (blur)="options.showErrors = true">
+        (blur)="options.showErrors = true"
+         (selectionChange)="updateValue($event)">
         <ng-template ngFor let-selectItem [ngForOf]="selectList">
           <mat-option *ngIf="!isArray(selectItem?.items)"
             [value]="selectItem?.value">
@@ -68,6 +72,34 @@ import { JsonSchemaFormService, buildTitleMap, isArray } from '@ng-formworks/cor
           </mat-optgroup>
         </ng-template>
       </mat-select>
+      <mat-select *ngIf="boundControl && options?.multiple"
+        [attr.aria-describedby]="'control' + layoutNode()?._id + 'Status'"
+        [attr.name]="controlName"
+        [disabled]="controlDisabled || options?.readonly"
+        [id]="'control' + layoutNode()?._id"
+        [multiple]="options?.multiple"
+        [placeholder]="options?.notitle ? options?.placeholder : options?.title"
+        [required]="options?.required"
+        [style.width]="'100%'"
+        [value]="controlValue"
+        (blur)="options.showErrors = true"
+        (selectionChange)="updateValue($event)">
+        <ng-template ngFor let-selectItem [ngForOf]="selectList">
+          <mat-option *ngIf="!isArray(selectItem?.items)"
+            [attr.selected]="selectItem?.value === controlValue"
+            [value]="selectItem?.value">
+            <span [innerHTML]="selectItem?.name"></span>
+          </mat-option>
+          <mat-optgroup *ngIf="isArray(selectItem?.items)"
+            [label]="selectItem?.group">
+            <mat-option *ngFor="let subItem of selectItem.items"
+              [attr.selected]="subItem?.value === controlValue"
+              [value]="subItem?.value">
+              <span [innerHTML]="subItem?.name"></span>
+            </mat-option>
+          </mat-optgroup>
+        </ng-template>
+      </mat-select>
       <span matSuffix *ngIf="options?.suffix || options?.fieldAddonRight"
         [innerHTML]="options?.suffix || options?.fieldAddonRight"></span>
       <mat-hint *ngIf="options?.description && (!options?.showErrors || !options?.errorMessage)"
@@ -92,6 +124,7 @@ export class MaterialSelectComponent implements OnInit, OnDestroy {
   boundControl = false;
   options: any;
   selectList: any[] = [];
+  selectListFlatGroup: any[] = [];
   isArray = isArray;
   readonly layoutNode = input<any>(undefined);
   readonly layoutIndex = input<number[]>(undefined);
@@ -103,18 +136,50 @@ export class MaterialSelectComponent implements OnInit, OnDestroy {
       this.options.titleMap || this.options.enumNames,
       this.options.enum, !!this.options.required, !!this.options.flatList
     );
+    //the selectListFlatGroup array will be used to update the formArray values
+    //while the selectList array will be bound to the form select
+    //as either a grouped select or a flat select
+    this.selectListFlatGroup = buildTitleMap(
+      this.options.titleMap || this.options.enumNames,
+      this.options.enum, !!this.options.required, true
+    )
     this.jsf.initializeControl(this, !this.options.readonly);
     if (!this.options.notitle && !this.options.description && this.options.placeholder) {
       this.options.description = this.options.placeholder;
     }
   }
 
+  deselectAll() {
+    this.selectListFlatGroup.forEach(selItem => {
+      selItem.checked = false;
+    })
+  }
+
+
+
   updateValue(event) {
+
     this.options.showErrors = true;
+    if (this.options.multiple) {
+      if (event.value.includes(null)) {
+        this.deselectAll();
+        //this.control.setValue([]);  // Reset the form control to an empty array
+        //this.selectList=JSON.parse(JSON.stringify(this.selectList));
+        this.jsf.updateArrayMultiSelectList(this, []);
+      } else {
+        this.selectListFlatGroup.forEach(selItem => {
+          selItem.checked = event.value.indexOf(selItem.value) >= 0 ? true : false;
+        })
+        this.jsf.updateArrayMultiSelectList(this, this.selectListFlatGroup);
+      }
+      return;
+    }
     this.jsf.updateValue(this, event.value);
   }
 
-  ngOnDestroy () {
-    this.jsf.updateValue(this, null);
+  ngOnDestroy() {
+    let nullVal=this.options.multiple?[null]:null;
+    this.formControl.reset(nullVal)
+    this.controlValue=null;
   }
 }
