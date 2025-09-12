d3153121d873ebb4a5311705eeb0cf3559518930
zahmo <zuser@nowhere.nw>
2025-09-12 17:02:10 +0200

    feat:updated exports with new jsonform core renderers

--- projects/ng-formworks-core/src/lib/json-schema-form.module.ts
+++ projects/ng-formworks-core/src/lib/json-schema-form.module.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JsonFormsModule } from '@jsonforms/angular';
 import { NoFrameworkModule } from './framework-library/no-framework.module';
 import { JsonSchemaFormComponent } from './json-schema-form.component';
+import { JsonformsFormComponent } from './jsonforms-form/jsonforms-form.component';
+import { BASIC_RENDERERS } from './jsonforms-renderers';
 import { WidgetLibraryModule } from './widget-library/widget-library.module';
-
 @NgModule({
   imports: [
     CommonModule, FormsModule, ReactiveFormsModule,
-    WidgetLibraryModule, NoFrameworkModule
+    WidgetLibraryModule, NoFrameworkModule,
+    JsonFormsModule
   ],
-  declarations: [JsonSchemaFormComponent],
-  exports: [JsonSchemaFormComponent, WidgetLibraryModule]
+  declarations: [...BASIC_RENDERERS,JsonSchemaFormComponent,JsonformsFormComponent],
+  exports: [...BASIC_RENDERERS,JsonSchemaFormComponent, WidgetLibraryModule,JsonformsFormComponent]
 })
 export class JsonSchemaFormModule {
 }
--- projects/ng-formworks-core/src/public_api.ts
+++ projects/ng-formworks-core/src/public_api.ts
@@ -2,20 +2,23 @@
  * Public API Surface of json-schema-form
  */
 
-export { JsonSchemaFormModule } from './lib/json-schema-form.module';
-export { TitleMapItem, ErrorMessages, JsonSchemaFormService } from './lib/json-schema-form.service';
-export { JsonSchemaFormComponent } from './lib/json-schema-form.component';
 export { Framework } from './lib/framework-library/framework';
 export { FrameworkLibraryService } from './lib/framework-library/framework-library.service';
+export { JsonSchemaFormComponent } from './lib/json-schema-form.component';
+export { JsonSchemaFormModule } from './lib/json-schema-form.module';
+export { ErrorMessages, JsonSchemaFormService, TitleMapItem } from './lib/json-schema-form.service';
+export { JsonformsFormComponent } from './lib/jsonforms-form/jsonforms-form.component';
+export * from './lib/jsonforms-renderers';
 export {
-    deValidationMessages,
-    enValidationMessages,
-    esValidationMessages,
-    frValidationMessages,
-    itValidationMessages,
-    ptValidationMessages,
-    zhValidationMessages
-  } from './lib/locale';
+  deValidationMessages,
+  enValidationMessages,
+  esValidationMessages,
+  frValidationMessages,
+  itValidationMessages,
+  ptValidationMessages,
+  zhValidationMessages
+} from './lib/locale';
+export * from './lib/shared';
 export * from './lib/widget-library';
 export * from './lib/widget-library/widget-library.module';
-export * from './lib/shared';
+
