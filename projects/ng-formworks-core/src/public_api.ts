fce8d80122b261d476be3905f9c7b0fbae240cdc
zahmo <zuser@nowhere.nw>
2025-10-03 00:50:08 +0200

    refactor:export NGFFormComponent

--- projects/ng-formworks-core/src/public_api.ts
+++ projects/ng-formworks-core/src/public_api.ts
@@ -4,7 +4,7 @@
 
 export { Framework } from './lib/framework-library/framework';
 export { FrameworkLibraryService } from './lib/framework-library/framework-library.service';
-export { JsonSchemaFormComponent } from './lib/json-schema-form.component';
+export { FormEngine, JsonSchemaFormComponent } from './lib/json-schema-form.component';
 export { JsonSchemaFormModule } from './lib/json-schema-form.module';
 export { ErrorMessages, JsonSchemaFormService, TitleMapItem } from './lib/json-schema-form.service';
 export { JsonformsFormComponent } from './lib/jsonforms-form/jsonforms-form.component';
@@ -18,6 +18,7 @@ export {
   ptValidationMessages,
   zhValidationMessages
 } from './lib/locale';
+export { NGFFormComponent } from './lib/ngf-form.component';
 export * from './lib/shared';
 export * from './lib/widget-library';
 export * from './lib/widget-library/widget-library.module';
