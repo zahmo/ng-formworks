f2026064693614a4d9625bec162c54974693d1e6
zahmo <zuser@nowhere.nw>
2025-11-24 11:02:44 +0200

    fix:wrapped ngOnDestroy logic in setTimeout for dynamic titles to update

--- projects/ng-formworks-core/src/lib/widget-library/input.component.ts
+++ projects/ng-formworks-core/src/lib/widget-library/input.component.ts
@@ -85,7 +85,12 @@ export class InputComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy () {
-    this.jsf.updateValue(this, null);
+    //needed to be done in timeout for when dynamic/condition based
+    //titles depend on the formControls value but the formControl
+    //is also destroyed
+    setTimeout(()=>{
+      this.jsf.updateValue(this, null);
+    })
   }
 
 }
