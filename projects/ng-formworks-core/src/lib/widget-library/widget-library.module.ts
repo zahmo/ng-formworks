d06696d7bded77d9ca95c4f857fbe37d5bbb1302
zahmo <zuser@nowhere.nw>
2025-12-18 15:20:43 +0200

    feat:created TextTemplatePipe to help with dynamic titles

--- projects/ng-formworks-core/src/lib/widget-library/widget-library.module.ts
+++ projects/ng-formworks-core/src/lib/widget-library/widget-library.module.ts
@@ -6,12 +6,13 @@ import { ElementAttributeDirective } from './element-attribute.directive';
 import { BASIC_WIDGETS } from './index';
 import { OrderableDirective } from './orderable.directive';
 import { StopPropagationDirective } from './stop-propagation.directive';
+import { TextTemplatePipe } from './text-template.pipe';
 
 @NgModule({
     imports: [CommonModule, FormsModule, ReactiveFormsModule,DragDropModule
  ],
-    declarations: [...BASIC_WIDGETS, OrderableDirective,ElementAttributeDirective,StopPropagationDirective],
-    exports: [...BASIC_WIDGETS, OrderableDirective,ElementAttributeDirective,StopPropagationDirective]
+    declarations: [...BASIC_WIDGETS, OrderableDirective,ElementAttributeDirective,StopPropagationDirective,TextTemplatePipe],
+    exports: [...BASIC_WIDGETS, OrderableDirective,ElementAttributeDirective,StopPropagationDirective,TextTemplatePipe]
 })
 export class WidgetLibraryModule {
 }
