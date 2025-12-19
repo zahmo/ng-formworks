b8397b582cc58954e8c95cb9ba7a4ae85109027b
zahmo <zuser@nowhere.nw>
2025-12-19 12:28:42 +0200

    fix:changed deprecated platformBrowserDynamic to platformBrowser

--- demo/main.ts
+++ demo/main.ts
@@ -1,9 +1,10 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
+
+import { platformBrowser } from '@angular/platform-browser';
 import { DemoModule } from './app/demo.module';
 import { environment } from './environments/environment';
 
 if (environment.production) { enableProdMode(); }
 
-platformBrowserDynamic().bootstrapModule(DemoModule, { applicationProviders: [provideZoneChangeDetection()], });
+platformBrowser().bootstrapModule(DemoModule );
